51ff720b273fe824b49f7ffb5104c99b1c7508e7
James Logan <jlogan03@gmail.com>
2023-05-14 15:39:32 -0400

    roll rust edition, dep versions, and sundials submodule commit forward to latest
    add static_libraries to features list. update simple_ode test for changes since sundials 6.x.
    
    Parse SUNDIALS version from bindings.rs (#1)
    
    * add manual run to build triggers to allow easier troubleshooting for some cases
    
    * configure tests based on sundials version
    
    * add rustfmt explicitly so that parsing version will work on beta and nightly
    
    * tweak rustfmt addition
    
    * use a less brittle parsing method
    
    * try removing rustfmt for nightly
    
    * add more debugging output
    
    * use tree-sitter to parse out the snudials major version
    
    * move the version parser to its own file
    
    clarify error messages for version parser
    
    run rustfmt on lib.rs
    
    undo autoformatting from rustfmt, Prettier, and BetterTOML. remove workflow dispatch trigger for actions that hopefully won't be needed in the future.
    
    put some more formatting back to manual form
    
    roll back more rustfmt formatting
    
    put more rustfmt formatting back to manual form

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,77 @@
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(test)]
+#[cfg(not(any(
+    sundials_major_version = "1",
+    sundials_major_version = "2",
+    sundials_major_version = "3",
+    sundials_major_version = "4",
+    sundials_major_version = "5"
+)))]
 mod tests {
     use crate::*;
     use core::ffi::c_void;
+    use core::mem::zeroed;
+    use core::ptr::null_mut;
 
     #[test]
+    // This just tests if the most basic of all programs works. More tests to come soon.
+    fn simple_ode() {
+        unsafe extern "C" fn rhs(
+            _t: realtype,
+            y: N_Vector,
+            dy: N_Vector,
+            _user_data: *mut c_void,
+        ) -> i32 {
+            *N_VGetArrayPointer(dy) = -*N_VGetArrayPointer(y);
+            0
+        }
+
+        unsafe {
+            let mut ctx: SUNContext = zeroed::<SUNContext>();
+            match SUNContext_Create(null_mut(), &mut ctx) {
+                x if x < 0 => {
+                    panic!("Solver context creation failed");
+                }
+                _ => {}
+            };
+
+            let y = N_VNew_Serial(1, ctx);
+            *N_VGetArrayPointer(y) = 1.0;
+
+            let mut cvode_mem = CVodeCreate(CV_ADAMS, ctx);
+
+            CVodeInit(cvode_mem, Some(rhs), 0.0, y);
+            CVodeSStolerances(cvode_mem, 1e-6, 1e-8);
+
+            let matrix = SUNDenseMatrix(1, 1, ctx);
+            let solver = SUNLinSol_Dense(y, matrix, ctx);
+
+            CVodeSetLinearSolver(cvode_mem, solver, matrix);
+
+            let mut t = 0f64;
+            CVode(cvode_mem, 1.0, y, &mut t, CV_NORMAL);
+            // y[0] is now exp(-1)
+
+            let result = (*N_VGetArrayPointer(y) * 1e6) as i32;
+            assert_eq!(result, 367879);
+
+            N_VDestroy(y);
+            CVodeFree(&mut cvode_mem);
+            SUNLinSolFree(solver);
+            SUNMatDestroy(matrix);
+            SUNContext_Free(&mut ctx);
+        }
+    }
+
+    #[test]
+    #[cfg(any(
+        sundials_major_version = "1",
+        sundials_major_version = "2",
+        sundials_major_version = "3",
+        sundials_major_version = "4",
+        sundials_major_version = "5"
+    ))]
     // This just tests if the most basic of all programs works. More tests to come soon.
     fn simple_ode() {
         unsafe extern "C" fn rhs(
